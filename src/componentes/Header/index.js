1e3a9fbc4d3edd66b08ea8fa1d399ce665f78229
Gabriel Notari <gabriel.notari@icloud.com>
2024-11-27 12:11:03 -0300

    first commit

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,23 @@
+
+import Logo from '../Logo'
+import OpcoesHeader from '../OpcoesHeader'
+import IconeHeader from '../IconesHeader'
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+     background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
+
+function Header () {
+    return(
+    <HeaderContainer> 
+        <Logo />
+        <OpcoesHeader />
+        <IconeHeader />
+    </HeaderContainer>    
+    )
+}
+
+export default Header
